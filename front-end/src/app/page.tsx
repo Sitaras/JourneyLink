ac030c7958282081dace469c6f6f6e5a3a2c8912
sitaras <sitaras@thinkdesquared.com>
2024-12-24 17:53:22 +0200

    feat: init fe web app

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Button>journeyLink</Button>
+    </div>
+  );
+}
